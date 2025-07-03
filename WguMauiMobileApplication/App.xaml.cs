93dda3a7572a136b149b0941efa768d4ca1f3676
user <user@WIN-2GG79G9RUK5>
2025-07-03 15:09:11 +0300

    First commit of entire maui .net8 project, including gitignore after successful android and windows emulation.

---
+++ WguMauiMobileApplication/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace WguMauiMobileApplication
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+
+        }
+    }
+}
---
+++ WguMauiMobileApplication/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WguMauiMobileApplication
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
